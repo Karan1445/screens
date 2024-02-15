1ff47f71f6bd39e1c69b194d16d26f9e3bb55efa
KRN <goheljitu164@gmai.com>
2024-02-15 20:21:13 +0530

    ndw

--- lib/list_view_2nd_file.dart
+++ lib/list_view_2nd_file.dart
@@ -27,77 +27,108 @@ class Screen2 extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Container(
-              margin: const EdgeInsets.fromLTRB(25, 20, 25, 0),
-              alignment: Alignment.center,
-              child: const Text(
-                "Pick Destination With Me.",
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.deepPurple,
-                    fontSize: 24),
-              )),
-          Expanded(
-            child: ListView(children: [
+          Column(
+            children: [
+              Container(
+                  margin: const EdgeInsets.fromLTRB(25, 20, 25, 0),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Pick Destination With Me.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.deepPurple,
+                        fontSize: 24),
+                  )),
+              Expanded(
+                child: ListView(children: [
 
-              Listimga("assets/images/maybe.jpg"),
-              Listimga("assets/images/1st.jpg"),
-              Listimga("assets/images/nsplsh_59386c436f545267485045~mv2_d_6000_4000_s_4_2.webp"),
-              Listimga("assets/images/3rd.jpg"),
-              Listimga("assets/images/night_mon.jpg")
+                  Listimga("assets/images/maybe.jpg"),
+                  Listimga("assets/images/night.jpg"),
+                  Listimga("assets/images/nsplsh_59386c436f545267485045~mv2_d_6000_4000_s_4_2.webp"),
+                  Listimga("assets/images/3rd.jpg"),
+                  Listimga("assets/images/night_mon.jpg")
 
 
-            ],),
-          )
+                ],),
+              ),
+
+            ],
+          ),
+          bottomButton(),
         ],
       ),
     );
   }
 
-  Container Listimga(String img) {
-    return Container(margin: const EdgeInsets.fromLTRB(25, 25, 23, 10),
-              child: Stack(
-                children: [
-                  Container(
-                      clipBehavior: Clip.antiAlias,
-                       decoration:  BoxDecoration(
-                      borderRadius: BorderRadius.circular(19) // Adjust the radius as needed
-                  ),
-                      child: Image.asset(img,
-                        fit: BoxFit.cover,height: 130,width: 500,)),
-
-
-
-                  Container(margin: const EdgeInsets.fromLTRB(18, 72, 10,0),alignment: Alignment.center,
-
-                    child: const Column(
-
-                      children: [
-
-                        Row(
-                          children: [
-                            Text("Enjoy The Lake Sunset View\nIn The Boat",
-                                style:TextStyle(color: Colors.white70,fontSize: 12),),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(IconData(0xe3ab, fontFamily: 'MaterialIcons'),
-                                color: Colors.redAccent,size: 11),
-                            Text(
-                              "Lugano ,Switzerland",
-                              style: TextStyle(
-                                  color: Colors.redAccent, fontWeight: FontWeight.w500,fontSize: 11),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+  Container bottomButton() {
+    return Container(alignment: Alignment.center,margin: const EdgeInsets.only(bottom: 30),
+          child: Column(mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                    elevation: MaterialStateProperty.all(0)),
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.white,
+                  child: Icon(const IconData(0xe3ab, fontFamily: 'MaterialIcons'),
+                      color: Colors.purpleAccent.shade200),
+                ),
               ),
-            );
+            ],
+          ),
+        );
+  }
+
+  GestureDetector Listimga(String img) {
+    return GestureDetector(onTap: (){},
+      child: Container(margin: const EdgeInsets.fromLTRB(25, 25, 23, 10),
+                child: Stack(
+                  children: [
+                    Container(
+                        clipBehavior: Clip.antiAlias,
+                         decoration:  BoxDecoration(
+                        borderRadius: BorderRadius.circular(19) // Adjust the radius as needed
+                    ),
+                        child: Image.asset(img,
+                          fit: BoxFit.cover,height: 130,width: 500,)),
+
+
+
+                    Container(margin: const EdgeInsets.fromLTRB(18, 72, 10,0),alignment: Alignment.center,
+
+                      child: const Column(
+
+                        children: [
+
+                          Row(
+                            children: [
+                              Text("Enjoy The Lake Sunset View\nIn The Boat",
+                                  style:TextStyle(color: Colors.white70,fontSize: 12),),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(IconData(0xe3ab, fontFamily: 'MaterialIcons'),
+                                  color: Colors.redAccent,size: 11),
+                              Text(
+                                "Lugano ,Switzerland",
+                                style: TextStyle(
+                                    color: Colors.redAccent, fontWeight: FontWeight.w500,fontSize: 11),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:screens/list_view_2nd_file.dart';
+import 'package:scr3/after_on_click_list.dart';
+import 'package:scr3/list_view_2nd_file.dart';
+// import 'package:screens/list_view_2nd_file.dart';
 
 void main()=>
     runApp(const MaterialApp(debugShowCheckedModeBanner: false,
